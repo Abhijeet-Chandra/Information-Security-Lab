97f7f6d235ee1436e54d74c9b65f2c8d83be5173
∇×𝐸 <86531959+Abhijeet-Chandra@users.noreply.github.com>
2026-08-01 22:27:18 +0530

    added autokey cipher code

--- Lab1/Q2.py
+++ Lab1/Q2.py
@@ -26,16 +26,75 @@ def vigenere_decrypt(ciphertext, key):
 
     return plaintext
 
+# C = (P + K) mod 26
+def autokey_encrypt(plaintext, key):
+    plaintext = plaintext.lower().replace(' ', '')
+    ciphertext = ''
+    for i in range(len(plaintext)):
+        p = ord(plaintext[i]) - ord('a')
+        if i == 0:
+            k = key
+        else:
+            k = ord(plaintext[i-1]) - ord('a')
+        c = (p + k) % 26
+        ciphertext += chr(c + ord('a'))
+    return ciphertext
+
+# P = (C - K) mod 26
+def autokey_decrypt(ciphertext, key):
+    ciphertext = ciphertext.lower().replace(' ', '')
+    plaintext = ''
+    for i in range(len(ciphertext)):
+        c = ord(ciphertext[i]) - ord('a')
+
+        if i == 0:
+            k = key
+        else:
+            k = ord(plaintext[i-1]) - ord('a')
+        p = (c - k) % 26
+        plaintext += chr(p + ord('a'))
+    return plaintext
+
+
+
 def main():
-    plaintext = input("Enter plaintext: ")
-    key = input("Enter key: ")
+    while True:
+        print("\n===== Polyalphabetic Cipher Menu =====")
+        print("1. Vigenere Cipher")
+        print("2. Autokey Cipher")
+        print("3. Exit")
 
-    ciphertext = vigenere_encrypt(plaintext, key)
+        choice = int(input("Enter your choice: "))
 
-    print("Ciphertext :", ciphertext)
+        if choice == 1:
+            plaintext = input("Enter plaintext: ")
+            key = input("Enter key: ")
 
-    decrypted = vigenere_decrypt(ciphertext, key)
+            ciphertext = vigenere_encrypt(plaintext, key)
 
-    print("Plaintext  :", decrypted)
+            print("Ciphertext :", ciphertext)
+
+            decrypted = vigenere_decrypt(ciphertext, key)
+
+            print("Plaintext  :", decrypted)
+
+        elif choice == 2:
+            plaintext = input("Enter plaintext: ")
+            key = int(input("Enter initial key: "))
+
+            ciphertext = autokey_encrypt(plaintext, key)
+
+            print("Ciphertext :", ciphertext)
+
+            decrypted = autokey_decrypt(ciphertext, key)
+
+            print("Plaintext  :", decrypted)
+
+        elif choice == 3:
+            print("Exiting...")
+            break
+
+        else:
+            print("Invalid choice!")
 
 main()
