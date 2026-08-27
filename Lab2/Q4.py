d04e80daed18cea7a77f16ad746901c8e5f007e5
∇×𝐸 <86531959+Abhijeet-Chandra@users.noreply.github.com>
2026-08-27 12:43:58 +0530

    Update Q4.py

--- Lab2/Q4.py
+++ Lab2/Q4.py
@@ -1,25 +1,17 @@
 from Crypto.Cipher import DES3
-from Crypto.Util.Padding import pad, unpad
+from Crypto.Util.Padding import pad,unpad
 
-message = "Classified Text"
-message = message.encode()
+key = b"1234567890ABCDEF12345678"
+message = b"Classified Text"
 
-key = bytes.fromhex(
-    "1234567890ABCDEF"
-    "234567890ABCDEF1"
-    "34567890ABCDEF12"
-)
+#encrypt
+des3 = DES3.new(key, DES3.MODE_ECB)
+ciphertext = des3.encrypt(pad(message,8))
 
-cipher = DES3.new(key, DES3.MODE_ECB)
+print("Ciphertext: ", ciphertext.hex())
 
-padded_message = pad(message, DES3.block_size)
+#decrypt
+des3 = DES3.new(key, DES3.MODE_ECB)
+plaintext = unpad(des3.decrypt(ciphertext),8)
 
-cipher_text = cipher.encrypt(padded_message)
-
-print("Cipher text: ", cipher_text.hex())
-
-decrypted = cipher.decrypt(cipher_text)
-
-decrypted_message = unpad(decrypted, DES3.block_size)
-
-print("Decrypted message: ", decrypted_message.decode())
+print("Decrypted: ",plaintext.decode())
