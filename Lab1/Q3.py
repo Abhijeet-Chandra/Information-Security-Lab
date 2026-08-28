02b95a13f19b34a36a4e94dfbb82cf10dbf7ebb9
∇×𝐸 <86531959+Abhijeet-Chandra@users.noreply.github.com>
2026-08-28 13:00:35 +0530

    Update Q3.py

--- Lab1/Q3.py
+++ Lab1/Q3.py
@@ -1,96 +1,68 @@
-def find_row(ch, mat):
-    for i in range(5):
-        for j in range(5):
-            if mat[i][j] == ch:
-                return i
-    return -1
 
-def find_col(ch, mat):
-    for i in range(5):
-        for j in range(5):
-            if mat[i][j] == ch:
-                return j
-    return -1
+def playfair_encrypt(text,key):
+    # remove spaces and convert to lowercase:
+    text = text.lower().replace(" ","").replace("j","i")
+    key = key.lower().replace("j","i")
 
-def playfair_cipher_encryption(plaintext, key):
-    plaintext = plaintext.lower().replace(' ', '')
-    key = key.lower()
+    #Create key without repeated letters:
+    key = "".join(dict.fromkeys(key))
 
-    plaintext = plaintext.replace('j', 'i')
-    key = key.replace('j', 'i')
+    #create playfair matrix:
+    alphabet = "abcdefghiklmnopqrstuvwxyz"
+    letters = key + "".join(c for c in alphabet if c not in key)
 
-    mat = []
-    row = -1
+    matrix = [letters[i:i+5] for i in range(0,25,5)]
 
-    st = set(key)
-    key = ''.join(dict.fromkeys(key))
-
-    #create a matrix
-    for i in range(len(key)):
-        if i % 5 == 0 :
-            mat.append([])
-            row += 1
-        mat[row].append(key[i])
-
-    # Fill remaining alphabet
-    for i in range(26):
-        ch = chr(i + ord('a'))
-        if ch == 'j':
-            continue
-        if ch not in st:
-            if sum(len(r) for r in mat) % 5 == 0:
-                mat.append([])
-            mat[-1].append(ch)
-
-    #prepare new plain text:
-    new_plaintext = ""
-
+    #prepare plaintext pairs:
+    pairs = []
     i = 0
-    while i < len(plaintext):
 
-        ch1 = plaintext[i]
+    while i < len(text):
 
-        if i + 1 >= len(plaintext):
-            new_plaintext += ch1 + "x"
+        a = text[i]
+
+        if i+1 == len(text):
+            pairs.append(a + "x")
             break
 
-        ch2 = plaintext[i+1]
+        b = text[i+1]
 
-        if ch1 == ch2:
-            new_plaintext += ch1 + "x"
+        if a == b:
+            pairs.append(a + "x")
             i += 1
         else:
-            new_plaintext += ch1 + ch2
-            i +=2
-    plaintext = new_plaintext
+            pairs.append(a + b)
+            i += 2
 
-    #iterate over plaintext and encrypt it finally
-    ciphertext = ""
-    for i in range(0, len(plaintext), 2):
-        ch1 = plaintext[i]
-        ch2 = plaintext[i+1]
+    cipher = ""
 
-        row1 = find_row(ch1, mat)
-        col1 = find_col(ch1, mat)
+    for a,b in pairs:
 
-        row2 = find_row(ch2, mat)
-        col2 = find_col(ch2, mat)
+        r1,r2,c1,c2 = 0,0,0,0
+        for r in range(5):
+            for c in range(5):
+                if matrix[r][c] == a:
+                    r1,c1 = r,c
+                if matrix[r][c] == b:
+                    r2,c2 = r,c
 
-        if row1 == row2:
-            ch1 = mat[row1][(col1+1)%5]
-            ch2 = mat[row2][(col2+1)%5]
-        elif col1 == col2:
-            ch1 = mat[(row1+1)%5][col1]
-            ch2 = mat[(row2+1)%5][col2]
+            #same row
+        if r1 == r2:
+            cipher +=matrix[r1][(c1 + 1) % 5]
+            cipher += matrix[r1][(c2 + 1) % 5]
+        elif c1 == c2:
+            cipher += matrix[(r1 + 1) % 5][c1]
+            cipher += matrix[(r2 + 1) % 5][c2]
         else:
-            ch1 = mat[row1][col2]
-            ch2 = mat[row2][col1]
+            cipher += matrix[r1][c2]
+            cipher += matrix[r2][c1]
 
-        ciphertext += ch1
-        ciphertext += ch2
-    return ciphertext
-
+    return cipher
 def main():
-    print(playfair_cipher_encryption("The key is hidden under the door pad", "GUIDANCE"))
+    print(playfair_encrypt(
+        "The key is hidden under the door pad",
+        "GUIDANCE"
+    ))
+
 
 main()
